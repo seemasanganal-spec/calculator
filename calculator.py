16d63bc0b0a2416e9eb0b046bfd58c8fa1fe8388
Seema R Sanganal <seemasanganal@gmail.com>
2026-09-18 16:04:51 +0530

    Added the multiplication and division

--- calculator.py
+++ calculator.py
@@ -6,7 +6,13 @@ num2 = float(input("Enter the second number: "))
 # Perform addition and subtraction
 sum_result = num1 + num2
 diff_result = num1 - num2
+multiply = num1 * num2
+division = num1/num2
 
 # Display the results
 print(f"The sum of {num1} and {num2} is: {sum_result}")
 print(f"The difference when subtracting {num2} from {num1} is: {diff_result}")
+print(f"The product of {num2} from {num1} is: {multiply}")
+print(f"The division {num2} from {num1} is: {division}")
+
+
